15cd6a59741ae3db11e3aeb0c8c61f0e2f45cbdf
Kristian Dupont <kristian@kristiandupont.com>
2021-06-23 15:03:48 +0000

    Set up

---
+++ index.js
@@ -0,0 +1,81 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: "module",
+  },
+
+  extends: [
+    "plugin:prettier/recommended", // https://github.com/prettier/eslint-plugin-prettier#recommended-configuration
+    "prettier/prettier",
+    "plugin:ramda/recommended",
+  ],
+
+  plugins: ["prettier", "jest", "ramda"],
+
+  overrides: [
+    {
+      files: ["*.js", "*.jsx"],
+      parser: "babel-eslint",
+      plugins: [],
+      rules: {},
+    },
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint/eslint-plugin"],
+      rules: {
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          { argsIgnorePattern: "^_" },
+        ],
+        "no-unused-vars": "off",
+      },
+    },
+    {
+      files: [
+        "*.{spec,test}.{js,ts,tsx}",
+        "**/__{mocks,tests}__/**/*.{js,ts,tsx}",
+      ],
+      env: {
+        jest: true,
+        "jest/globals": true,
+      },
+      rules: {
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error",
+        "import/no-extraneous-dependencies": "off",
+        "import/no-duplicates": "off",
+        "no-underscore-dangle": "off",
+        quotes: [
+          1,
+          "single",
+          { avoidEscape: true, allowTemplateLiterals: true },
+        ],
+      },
+    },
+  ],
+
+  rules: {
+    "prettier/prettier": "error",
+    "no-unused-expressions": "error",
+    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "no-implicit-coercion": "error",
+    "no-unused-labels": "error",
+    "no-process-env": "error",
+    "no-restricted-syntax": [
+      "error",
+      "ForInStatement",
+      "LabeledStatement",
+      "WithStatement",
+    ],
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    "no-restricted-modules": ["error", "crypto"],
+    "no-restricted-imports": ["error", "crypto"],
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "arrow-body-style": ["error", "as-needed"],
+  },
+};
